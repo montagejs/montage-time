5651c0648177cb9420757ad50085adaca4a1e440
Corentin Debost <codebost@gmail.com>
2018-02-26 17:19:03 -0800

    Remove tasks from month calendar

--- ui/calendar.reel/calendar-widget.reel/calendar-widget-day.reel/calendar-widget-day.js
+++ ui/calendar.reel/calendar-widget.reel/calendar-widget-day.reel/calendar-widget-day.js
@@ -1,37 +1,6 @@
 var Component = require("montage/ui/component").Component;
 
 exports.CalendarWidgetDay = Component.specialize({
-    _tasks: {
-        value: null
-    },
-
-    tasks: {
-        get: function() {
-            return this._tasks;
-        },
-        set: function(tasks) {
-            if (this._tasks !== tasks) {
-                this._tasks = tasks;
-                this._filterDistinctTasks();
-            }
-        }
-    },
-
-    _taskCategories: {
-        value: null
-    },
-
-    taskCategories: {
-        get: function() {
-            return this._taskCategories;
-        },
-        set: function(taskCategories) {
-            if (this._taskCategories !== taskCategories) {
-                this._taskCategories = taskCategories;
-                this._filterDistinctTasks();
-            }
-        }
-    },
 
     _data: {
         value: null
@@ -48,10 +17,6 @@ exports.CalendarWidgetDay = Component.specialize({
         }
     },
 
-    _allTasksVisible: {
-        value: false
-    },
-
     _setMaxDisplayedLines: {
         value: function () {
             if (document.documentElement.clientHeight > 920) {
@@ -62,19 +27,11 @@ exports.CalendarWidgetDay = Component.specialize({
         }
     },
 
-    templateDidLoad: {
-        value: function() {
-            this.super();
-        }
-    },
-
     enterDocument: {
         value: function(isFirstTime) {
             this.super();
             if (isFirstTime) {
                 this.element.addEventListener("click", this.handleClick.bind(this));
-                this.addRangeAtPathChangeListener("taskCategories", this, "_filterDistinctTasks");
-                this.addRangeAtPathChangeListener("tasks", this, "_filterDistinctTasks");
                 window.addEventListener("resize", this, false);
             }
             this._setMaxDisplayedLines();
@@ -90,42 +47,6 @@ exports.CalendarWidgetDay = Component.specialize({
     handleResize: {
         value: function() {
             this._setMaxDisplayedLines();
-        }
-    },
-
-    _handleTaskCategoriesChange: {
-        value: function() {
-            this._filterDistinctTasks();
-        }
-    },
-
-    _filterDistinctTasks: {
-        value: function() {
-            if (this._tasks && this._taskCategories) {
-                var self = this;
-                this.distinctTasks = this._tasks
-                    .filter(function(x) {
-                        var category = self._taskCategories.filter(function(y) {
-                            return x.task.task === y.value;
-                        })[0];
-                        return category;
-                    })
-                    .map(function(x) {
-                        return x.task;
-                    })
-                    .reduce(function(a,b) {
-                        if (a.indexOf(b) == -1) {
-                            a.push(b);
-                        }
-                        return a;
-                    }, []);
-            }
-        }
-    },
-
-    handleMoreTasksButtonAction: {
-        value: function () {
-            this._allTasksVisible = !this._allTasksVisible;
         }
     },
 
